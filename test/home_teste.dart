5685b12f80fc3df7654c9bda0fcb56bcc649ab8d
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-05-31 16:54:22 -0300

    refactor: import

--- test/home_teste.dart
+++ test/home_teste.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
